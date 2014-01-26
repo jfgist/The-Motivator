9fb8bd5ccc03ce9a35759f1e12ad14758e97b3db
jfgist <james.gist@gmail.com>
2014-01-26 00:18:05 +0000

    adding new sound files

--- soundboard2.py
+++ soundboard2.py
@@ -9,24 +9,21 @@ pygame.display.set_caption("Sound Board II")
 screen = pygame.display.set_mode((640, 480), 0, 32)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # LED's
 GPIO.setup(9,GPIO.OUT)
 GPIO.setup(10,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
+#GPIO.setup(17,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 
 GPIO.output(9,True)
 GPIO.output(10,True)
-GPIO.output(17,True)
+#GPIO.output(17,True)
 GPIO.output(27,True)
 
 
@@ -34,22 +31,19 @@ time_stamp = time.time()
 
 bTime = 300
 
-sound0 = pygame.mixer.Sound("nope.wav")
-sound1 = pygame.mixer.Sound("worksmarter.wav")
-sound2 = pygame.mixer.Sound("CastleThunder.wav")
-sound3 = pygame.mixer.Sound("donottellme.wav")
-sound4 = pygame.mixer.Sound("justmakeitwork.wav")
-sound5 = pygame.mixer.Sound("shipit.wav")
-sound6 = pygame.mixer.Sound("WilhelmScream.wav")
-sound7 = pygame.mixer.Sound("applause.wav")
+sound0 = pygame.mixer.Sound("DoItNowCheckItIn.wav")
+sound1 = pygame.mixer.Sound("DoItNow.wav")
+sound2 = pygame.mixer.Sound("CheckItIn.wav")
+sound3 = pygame.mixer.Sound("justmakeitwork.wav")
+sound4 = pygame.mixer.Sound("applause.wav")
 
 def PlaySound(channel):
-  print "Entering PlaySound"
+#  print "Entering PlaySound"
 
   global time_stamp
   time_now = time.time()
 
-  print "Time Stamp"
+#  print "Time Stamp"
 #  print time_stamp
 #  print "Time Now:" 
 #  print time_now
@@ -58,33 +52,23 @@ def PlaySound(channel):
   
   if (time_now - time_stamp) >= .3:
     print channel
-    if channel == 14:
+    if channel == 17:
       sound0.play()
-    if channel == 15:
-      sound1.play()
-    if channel == 18:
-      sound2.play()
-    if channel == 23:
-      sound3.play()
-    if channel == 24:
-      sound4.play()
-    if channel == 25:
-      sound5.play()
-    if channel == 8:
-      sound6.play()
     if channel == 7:
-      sound7.play()
+      sound1.play()
+    if channel == 8:
+      sound2.play()
+    if channel == 25:
+      sound3.play()
+    if channel == 23:
+      sound4.play()
   time_stamp = time.time()
   
-GPIO.add_event_detect(14, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-GPIO.add_event_detect(15, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-GPIO.add_event_detect(18, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-GPIO.add_event_detect(23, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-GPIO.add_event_detect(24, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-GPIO.add_event_detect(25, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-GPIO.add_event_detect(8, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
+GPIO.add_event_detect(17, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
 GPIO.add_event_detect(7, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
-
+GPIO.add_event_detect(8, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
+GPIO.add_event_detect(25, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
+GPIO.add_event_detect(23, GPIO.RISING, callback=PlaySound, bouncetime=bTime)
 
 while True:
 
